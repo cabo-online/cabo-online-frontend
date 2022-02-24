93b27e3d0b70d8ca342a42d84a1622303c855d85
jhayes99823 <hayesja99@gmail.com>
2022-02-24 15:00:09 -0500

    add some very basic form to the home page and navigate to waiting room page after getting game code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
-import { Button, Form } from 'react-bootstrap';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { createNewGame } from '../store/actions';
 import { RootState } from '../store/reducers';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+    const navigate = useNavigate();
     const [name, setName] = useState('');
+    const [gameCode, setGameCode] = useState('');
 
     const state = useSelector((state: RootState) => state.game);
     const dispatch = useDispatch();
@@ -14,27 +17,69 @@ function Home() {
         e.preventDefault();
         setName(e.target.value);
     }
+
+    const onChangeGameCode = (e: any) => {
+        e.preventDefault();
+        setGameCode(e.target.value);
+    }
   
     const submitName = (e: any) => {
         e.preventDefault();
         dispatch(createNewGame(name));
+        navigate('/waiting-room');
     };
 
   return (
     <div>
-        {console.log('state', state)}
-        <Form onSubmit={(e) => submitName(e)}>
-            <Form.Group className="mb-3" controlId="name">
-                <Form.Label>Name</Form.Label>
-                <Form.Control onChange={(e) => onChange(e)} type="text" placeholder="Enter name" />
-            </Form.Group>
-
-            <Button variant="primary" type="submit">
-                Create Game
-            </Button>
-         </Form>
-         { state.data?.game_code ? state.data?.game_code : 'nothing yet ' }
-         { state.data?.players ? state.data?.players[0] : 'nothing yet '}
+        <Container>
+            <Row className="justify-content-center my-4">
+                Welcome to Cabo Online!
+            </Row>
+            <Row className="justify-content-center">
+                <Col>
+                    <Form onSubmit={(e) => submitName(e)}>
+                        <Row className="justify-content-center">
+                            <Col className="col-4">
+                                <Form.Group className="mb-3" controlId="name">
+                                    <Form.Label>Name</Form.Label>
+                                    <Form.Control onChange={(e) => onChange(e)} type="text" placeholder="Enter name" />
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        <Row className="justify-content-center">
+                            <Col className="col-4">
+                                <Form.Group className="mb-3" controlId="game-code">
+                                    <Form.Label>Game Code</Form.Label>
+                                    <Form.Control onChange={(e) => onChangeGameCode(e)} type="text" placeholder="Enter game code" />
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        <Row className="justify-content-center my-2">
+                            <Col className="justify-content-center col-4">
+                                <Button variant="primary" type="submit">
+                                    Create Game
+                                </Button>
+                            </Col>
+                            <Col className="justify-content-center col-4">
+                                <Button variant="primary" type="submit">
+                                    Join Game
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Form>
+                </Col>
+            </Row>
+            <Container>
+                <Row className="justify-content-center my-5"> 
+                    <Col className="col-4">
+                        { state.data?.game_code ? state.data?.game_code : 'nothing yet ' }
+                    </Col>
+                    <Col className="col-4">
+                        { state.data?.players ? state.data?.players[0] : 'nothing yet '}
+                    </Col>
+                </Row>
+            </Container>
+        </Container>
     </div>
   )
 }
--- src/pages/WaitingRoom.tsx
+++ src/pages/WaitingRoom.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/reducers';
 
-function WaitingRoom() {
+function WaitingRoom(props: any) {
+  const state = useSelector((state: RootState) => state.game);
   return (
-    <div>WaitingRoom</div>
+    <div>
+      {console.log('state', state)}
+      <Container>
+            <Row className="justify-content-center my-5"> 
+                <Col className="col-4">
+                    { state.data?.game_code ? state.data?.game_code : 'nothing yet ' }
+                </Col>
+                <Col className="col-4">
+                    { state.data?.players ? state.data?.players[0] : 'nothing yet '}
+                </Col>
+            </Row>
+        </Container>
+    </div>
   )
 }
 
